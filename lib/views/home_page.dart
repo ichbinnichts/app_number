1287c75382416b5f9670aaab1aa7e6603dac910b
Nathan Faria <nathanfaria607@gmail.com>
2024-05-29 08:28:54 -0300

    refactor: remove os folders

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(Colors.green, 'App Numérico'),
+      appBar: appBar(Colors.green, '# App Numérico'),
     );
   }
 }
